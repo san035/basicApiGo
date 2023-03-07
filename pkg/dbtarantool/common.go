527ef5b971f3f4e8fb967d50914d36a6136c144e
Alexanrd Skorohodov <askorn@mail.ru>
2023-03-07 10:31:47 +0300

    * dbtarantool

---
+++ pkg/dbtarantool/common.go
@@ -0,0 +1,41 @@
+package dbtarantool
+
+import (
+	"fmt"
+	"github.com/san035/basicApiGo/pkg/logger"
+)
+
+// ListInterfaceToStruct конвертация списка интерфейсов listInterface в другую структуру
+// mapNameColumnToData - map
+// ключи id из списка listInterface
+// значения - ссылки на значения в структуре
+func ListInterfaceToStruct(listInterface *[]interface{}, mapNameColumnToData map[int]interface{}) error {
+	countColumnData := len((*listInterface))
+	for idColumn, distValue := range mapNameColumnToData {
+		if idColumn >= countColumnData {
+			continue
+		}
+
+		switch valueTo := distValue.(type) {
+		case *string:
+			*valueTo = (*listInterface)[idColumn].(string)
+		case *int64:
+			// приемник с типом int64
+			switch valueFrom := (*listInterface)[idColumn].(type) {
+			case uint64:
+				*valueTo = int64(valueFrom)
+			case int64:
+				*valueTo = valueFrom
+			default:
+				return logger.WrapWithDeep1(&error_parse_data_user, idColumn)
+			}
+		case *uint64:
+			*valueTo = (*listInterface)[idColumn].(uint64)
+		case *CustomTime: // uint64
+			*valueTo = CustomTime((*listInterface)[idColumn].(uint64))
+		default:
+			return logger.WrapWithDeep1(&error_parse_data_user, map[string]interface{}{"Колонка:": idColumn, "Тип": fmt.Sprintf("%T", distValue)})
+		}
+	}
+	return nil
+}
